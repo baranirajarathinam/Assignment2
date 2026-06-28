7ef1c3911512153e96a2b566d6755f48f90a3fa9
Your Name <you@example.com>
2026-06-28 15:58:54 +0000

    Added Log line in main method

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package org.example.evaluations;
 
 public class Main {
     public static void main(String args[]) {
+	    System.out.println("Printing shapes");
         Triangle triangle = new Triangle();
         triangle.print();
         Circle circle = new Circle();
